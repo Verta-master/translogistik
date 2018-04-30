565276e977d38e7d547c49e130b041bc7ba7814e
Verta-master <vertyagina@gmail.com>
2018-04-30 11:31:40 +0600

    JS

--- js/script.js
+++ js/script.js
@@ -4,21 +4,56 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 })
 
-//Header on scroll
+//Scroll to top
 $(window).scroll(function(){
   if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-//    $(".header").addClass('header--scroll');
     $(".btn-top").fadeIn();
   } else {
-//    $(".header").removeClass('header--scroll');
     $(".btn-top").fadeOut();
   };
 });
 
-//scroll to top
 $(".btn-top").click(function () {
   $("body,html").animate({
     scrollTop: 0
   }, 400);
   return false;
 });
+
+//Scroll to menu anchor
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+  //smoothscroll
+  $('.menu__item a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('.menu__item a').each(function () {
+        $(this).removeClass('menu__link--active');
+    })
+    $(this).addClass('menu__link--active');
+
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+        $(document).on("scroll", onScroll);
+    });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.menu__link').each(function () {
+      event.preventDefault();
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.menu__link').removeClass("menu__link--active");
+          currLink.addClass("menu__link--active");
+      }
+  });
+}
