346ee692c979d78ce12d04cd7e3ea31e0891db05
Verta-master <vertyagina@gmail.com>
2018-04-30 14:35:36 +0600

    Update 2018-04-30T08:35:32.127Z

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 })
 
+if (window.innerWidth < 766) {
+  $('.menu__link').click(function() {
+    $('.menu__list').slideToggle();
+    $('.menu').removeClass('menu--opened');
+  })
+}
+
 //Scroll to top
 $(window).scroll(function(){
   if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
